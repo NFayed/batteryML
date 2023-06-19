f858a82f713769d2984c161dd192d731be78d190
NFayed <norhan.fayed1412@gmail.com>
2023-06-19 23:47:04 +0300

    test

--- battery.py
+++ battery.py
@@ -2,44 +2,45 @@ import pandas as pd
 from sklearn.linear_model import LinearRegression
 import pickle
 
-def train_model():
-    # importing data
-    df = pd.read_csv("battery capacity.csv", usecols=['Cycles / Days of Aging Test','Capacity [Ah]'])
+# def train_model():
+#     # importing data
+#     df = pd.read_csv("battery capacity.csv", usecols=['Cycles / Days of Aging Test','Capacity [Ah]'])
+#
+#     X = df.drop('Capacity [Ah]', axis=1)
+#     y = df['Capacity [Ah]']
+#
+#     # creating a regression model
+#     model = LinearRegression()
+#
+#     # fitting the model
+#     model.fit(X, y)
+#
+#     # Save the trained model
+#     with open('model.pkl', 'wb') as file:
+#         pickle.dump(model, file)
+#
+# def load_model():
+#     # Load the trained model
+#     with open('model.pkl', 'rb') as file:
+#         model = pickle.load(file)
+#     return model
 
-    X = df.drop('Capacity [Ah]', axis=1)
-    y = df['Capacity [Ah]']
-
-    # creating a regression model
-    model = LinearRegression()
-
-    # fitting the model
-    model.fit(X, y)
-
-    # Save the trained model
-    with open('model.pkl', 'wb') as file:
-        pickle.dump(model, file)
-
-def load_model():
+def predict_value():
     # Load the trained model
-    with open('model.pkl', 'rb') as file:
-        model = pickle.load(file)
-    return model
-
-def predict_value(data):
-    # Load the trained model
-    model = load_model()
+    #model = load_model()
 
     # Make prediction using the loaded model
-    X_test = data
-    prediction = model.predict(X_test)
+    #X_test = data
+    #prediction = model.predict(X_test)
 
-    voltage = 400
-    prediction = prediction * voltage / 1000
+    #voltage = 400
+    #prediction = prediction * voltage / 1000
 
-    return prediction
+    #return prediction
+    print('ok')
 
 # Train the model and save it
-train_model()
+#train_model()
 
 # Test the predict_value function
 #data = 50
--- main.py
+++ main.py
@@ -3,14 +3,17 @@ from battery import predict_value
 import numpy as np
 app = Flask(__name__)
 
-@app.route('/predict/<data>', methods=['POST'])
-def predict(data):
+@app.route('/predict', methods=['POST'])
+def predict():
+    #def predict(data):
     #data = request.get_json().get('data')  # Get the 'data' field from the request's JSON body
-    fixeddata = [[float(data)]]
-    result = predict_value(fixeddata)
-    reshaped_data = np.array(result).reshape(-1, 1).tolist()
+    #fixeddata = [[float(data)]]
+    #result = predict_value(fixeddata)
+    print('ok')
+    return jsonify({'prediction': 1})
+    #reshaped_data = np.array(result).reshape(-1, 1).tolist()
     # Call the predict_value function with the input data
-    return jsonify({'prediction': reshaped_data})  # Return the prediction as a JSON response
+    #return jsonify({'prediction': reshaped_data})  # Return the prediction as a JSON response
 
 if __name__ == '__main__':
     app.run()
